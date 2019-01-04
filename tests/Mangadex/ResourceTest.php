4bd82fad2f9ad0c13ae01e4ca27834ada4330969
railken <railken@protonmail.ch>
2019-01-04 22:29:27 +0100

    update resource test

--- tests/Mangadex/ResourceTest.php
+++ tests/Mangadex/ResourceTest.php
@@ -11,7 +11,12 @@ class ResourceTest extends TestCase
         $api = new MangadexApi();
         $result = $api->resource(31477)->get();
 
-        $this->assertEquals('Solo Leveling', $result->name);
         $this->assertTrue($result->chapters->count() > 0);
+
+        $this->assertEquals("https://mangadex.org/title/31477", $result->url);
+        $this->assertEquals("31477", $result->uid);
+        $this->assertEquals('Solo Leveling', $result->name);
+        $this->assertEquals('Ongoing', $result->status);
+
     }
 }
